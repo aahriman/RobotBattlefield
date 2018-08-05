d875f52d272aa70688d8230e949e729a91f778c6
Vojtěch Sejkora <vojtech.sejkora@seznam.cz>
2018-08-05 12:05:07 +0200

    Add RoboTemplate

---
+++ RobotTemplate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BaseLibrary.battlefield;
+using BaseLibrary.communication.command.common;
+using BaseLibrary.communication.command.miner;
+using BaseLibrary.communication.command.tank;
+using BaseLibrary.communication.command.repairman;
+using BaseLibrary.communication.command.handshake;
+using BaseLibrary.equipment;
+using BaseLibrary.utils.euclidianSpaceStruct;
+using BaseLibrary.utils;
+
+using ClientLibrary.robot;
+using ClientLibrary.config;
+
+namespace RobotTemplate {
+    class Program {
+        static void Main(string[] args) {
+            ClientRobot.Connect(args);
+            Tank mujRobot = new Tank("My first tank", ClientRobot.TEAM_NAME);
+
+            /* Odkud níže piště kód*/
+        }
+    }
+}
