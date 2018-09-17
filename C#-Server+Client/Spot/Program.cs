c80a4aa55c16a19f020427235e2f0e53752e6c5a
Vojtěch Sejkora <vojtech.sejkora@seznam.cz>
2018-09-17 11:57:12 +0200

    Update version

--- C#-Server+Client/Spot/Program.cs
+++ C#-Server+Client/Spot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using BaseLibrary.communication.command.common;
